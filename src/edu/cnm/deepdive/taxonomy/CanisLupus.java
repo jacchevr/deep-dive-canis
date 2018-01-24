c094d308292790011e407e0e68c476744bc0e60f
jacchevr <jacchevr@gmail.com>
2018-01-24 15:47:29 -0700

    Initial commit of Canis project.

---
+++ src/edu/cnm/deepdive/taxonomy/CanisLupus.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.taxonomy;
+
+public class CanisLupus extends Canis {
+
+  @Override
+  public void hunt() {
+    System.out.println("Hunt animals in packs.");
+  }
+
+  public void speak () {
+    System.out.println("Howl!"); 
+  }
+  
+  public static void describe() {
+    System.out.println("Canis lupus is the species of gray wolves.");
+  }
+}
---
+++ src/edu/cnm/deepdive/taxonomy/Kennel.java
@@ -0,0 +1,52 @@
+/*
+ *Kennel.java 
+ */
+package edu.cnm.deepdive.taxonomy;
+
+/**
+ * This class exercises the {@link Canis}, {@link CanisLupus}, 
+ * and {@link CanisLupusFamiliaris} classes to demonstrate inheritance and polymorphism. This models
+ * binomial taxonomy of genus and species, as well as sub-species.
+ * 
+ * @author jacqueschevrier
+ */
+public class Kennel {
+
+  /**
+   * Runs a test using the (@link CanisLupus} and 
+   * {@link CanisLupusFamiliaris} classes. Command line
+   * arguments are ignored.
+   * 
+   * @param args    Command line arguments (ignored).
+   */
+  public static void main(String[] args) {
+    CanisLupus whiteFang = new CanisLupus();
+    CanisLupus spot = new CanisLupusFamiliaris(false);
+    CanisLupus fido = new CanisLupusFamiliaris(true);
+    System.out.println("White Fang:");
+    showOff(whiteFang);
+    System.out.println("Spot:");
+    showOff(spot);
+    System.out.println("Fido:");
+    showOff(fido);
+  }
+  
+  /**
+   * Invokes a number of methods on the {@link CanisLupus}
+   * instance passed to it as a parameter. If the instance
+   * type is actually {@link CanisLupusFamiliaris}, then
+   * the {@link CanisLupusFamiliaris#isGoodBoy()} method
+   * is also invoked.
+   * 
+   * @param wolf    instance exercised.
+   */
+  public static void showOff(CanisLupus wolf) {
+    wolf.describe();
+    wolf.hunt();
+    wolf.speak();
+    if (wolf instanceof CanisLupusFamiliaris) {
+      System.out.printf("Is a good boy:%b%n", ((CanisLupusFamiliaris) wolf).isGoodBoy());
+    }
+  }
+
+}
